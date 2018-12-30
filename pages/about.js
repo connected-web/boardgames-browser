e858d68df25eb24d3bd9c2b38bd9ef02f64eb71f
John Beech <johnbeech.github@mkv25.net>
2018-12-30 17:34:46 +0000

    Style links, remove <description> tag

--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,10 @@ import Layout from '../components/PageLayout.js'
 
 const About = (props) => (
   <Layout>
-    <heading>About</heading>
-    <description>
+    <h1>About</h1>
+    <div className='description'>
       <p>A browser for personalized boardgame data, sourced from boardgames-api.calisaurus.net and other locations.</p>
-    </description>
+    </div>
   </Layout>
 )
 
