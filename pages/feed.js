96017ce76b8f1033387383dd0a2e4fbf1d376c7c
John Beech <johnbeech.github@mkv25.net>
2019-01-03 13:14:47 +0000

    Add chart.js support; render a feed chart showing co-op games played by month

--- pages/feed.js
+++ pages/feed.js
@@ -1,13 +1,15 @@
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
 import Layout from '../components/PageLayout.js'
+import Chart from 'react-chartjs2'
 
 const Feed = (props) => (
   <Layout>
     <h1>Board Games Feed</h1>
     <div className='feed'>
-      {props.feed.map((game) => (
-        <div className='feed item' key={game.name}>
+      <Chart data={props.chartData} options={props.chartOptions} type='bar' />
+      {props.feed.map((game, index) => (
+        <div className='feed item' key={[game.name, '_', index].join('')}>
           <h2>{game.name}</h2>
           <div className='stat date'>
             <label>Date</label>
@@ -50,8 +52,42 @@ Feed.getInitialProps = async function () {
 
   console.log(`Game feed fetched. Count: ${data.feed.length}`)
 
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  const gamesPlayedByMonth = data.feed.reduce((acc, item) => {
+    const monthCode = item.date.substring(5, 7)
+    const month = acc[monthCode] || { monthCode, items: [] }
+    month.items.push(item)
+    acc[monthCode] = month
+    return acc
+  }, {})
+
+  const sortByMonthCode = (a, b) => Number.parseInt(a.monthCode) - Number.parseInt(b.monthCode)
+  const coOpGamesPlayedByMonth = Object.values(gamesPlayedByMonth).sort(sortByMonthCode).map(n => n.items.filter(x => x.coOp === 'Yes').length)
+
+  const chartData = {
+    labels: months,
+    datasets: [{
+      label: "Co-op Games Played",
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: coOpGamesPlayedByMonth
+    }]
+  }
+
+  const chartOptions = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          suggestedMin: 0
+        }
+      }]
+    }
+  }
+
   return {
-    feed: data.feed
+    feed: data.feed,
+    chartData,
+    chartOptions
   }
 }
 
