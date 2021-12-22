b9698c68b2e81fa360edcabcdc3911c311f1b6d1
John Beech <johnbeech.github@mkv25.net>
2021-12-22 23:49:17 +0000

    Add buttons to Create Play Record form as per original design

--- docs/.vuepress/components/CreatePlayRecords.vue
+++ docs/.vuepress/components/CreatePlayRecords.vue
@@ -5,14 +5,45 @@
           <h3>Which game? <span class="default info">(Required Field!)</span></h3>
           <input type="text" v-model="title" placeholder="The name of the game"  />
          
-          <h3>When was the game played? <span class="default info">(Default: today)</span></h3>
-          <input type="text" v-model="date" placeholder="dd/mm/yyyy" />
+          <h3>When was the game played? <span class="default info">(Default: today, Format: dd/mm/yyyy)</span></h3>
+          <input type="text" v-model="date" :placeholder="today" />
 
           <h3>Was this game co-operative? <span class="default info">(Default: no)</span></h3>
-          <input type="text" v-model="coop" placeholder="Yes / No" />
+          <div class="row">
+            <div :class="coopClass('yes')" v-on:click="selectCoop('yes')">
+              <label for="coop-yes">Yes</label>
+            </div>
+            <div :class="coopClass('no')" v-on:click="selectCoop('no')">
+              <label for="coop-no">No</label>
+            </div>
+          </div>
           
           <h3>Who won? <span class="default info">(Default: draw)</span></h3>
-          <input type="text" v-model="winner" placeholder="Who won?" />
+          <div v-if="coop === 'yes'" class="row">
+            <div :class="winnerClass('win')" v-on:click="winner = 'win'">
+              <label for="coop-yes">Win</label>
+            </div>
+            <div :class="winnerClass('loss')" v-on:click="winner = 'loss'">
+              <label for="coop-no">Loss</label>
+            </div>
+            <div :class="winnerClass('draw')" v-on:click="winner = 'draw'">
+              <label for="coop-no">Draw</label>
+            </div>
+          </div>
+          <div v-else class="row">
+            <div :class="winnerClass('Hannah')" v-on:click="winner = 'Hannah'">
+              <label for="coop-yes">Hannah</label>
+            </div>
+            <div :class="winnerClass('John')" v-on:click="winner = 'John'">
+              <label for="coop-no">John</label>
+            </div>
+            <div :class="winnerClass('draw')" v-on:click="winner = 'draw'">
+              <label for="coop-no">Draw</label>
+            </div>
+            <div :class="winnerClass('other')" v-on:click="winner = 'other'">
+              <label for="coop-no">Other</label>
+            </div>
+          </div>
           
           <h3>Number of Players <span class="default info">(Default: 2)</span></h3>
           <input type="text" v-model="noOfPlayers" placeholder="How many players were there?" />
@@ -21,7 +52,7 @@
         </form>
         <br />
         <b>Data preview</b>
-        <pre>{{JSON.stringify({ title, date, coop, winner, noOfPlayers }, null, 2)}}</pre>
+        <pre>{{JSON.stringify({ dataToSend }, null, 2)}}</pre>
         <p v-if="message">{{message}}</p>
       </div>    
 </template>
@@ -29,6 +60,7 @@
 <script>
 import axios from 'axios'
 import sharedModel from './src/sharedModel'
+import dayjs from 'dayjs'
 
 const { boardgamesSamApiUrl } = sharedModel.state
 
@@ -37,30 +69,52 @@ export default {
     return {
       title: '',
       date: '',
-      coop: '',
+      coop: 'no',
       winner: '',
       noOfPlayers: '',
       message: ''
     }
   },
+  computed: {
+    today() {
+      const now = new Date()
+      return dayjs(now).format('DD/MM/YYYY')
+    },
+    dataToSend() {
+      return {
+        title: this.title,
+        date: this.date || this.today,
+        coop: this.coop || 'no',
+        winner: this.winner || 'draw',
+        noOfPlayers: Number.parseInt(this.noOfPlayers) || 2,
+      }
+    }
+  },
   methods: {
+    coopClass(value) {
+      const selected = value === this.coop
+      const className = selected ? 'selected' : 'deselected'
+      return ['option', className].join(' ')
+    },
+    winnerClass(value) {
+      const selected = value === this.winner
+      const className = selected ? 'selected' : 'deselected'
+      return ['option', className].join(' ')
+    },
+    selectCoop(value) {
+      this.coop = value
+      this.winner = ''
+    },
     async handleSubmit(event) {
       event.preventDefault()
       console.log(`You played ${this.title} on ${this.date}. ${this.winner} was the winner!`)
-      const dataToSend = {
-        title: this.title,
-        date: this.date,
-        coop: this.coop,
-        winner: this.winner,
-        noOfPlayers: this.noOfPlayers,
-      }
       const url = `${boardgamesSamApiUrl}/playrecords/create`
       const axiosConfig = {
         headers: sharedModel.getAuthHeaders()
       }
       try {
         console.log('Do the thing')
-        await axios.post(url, dataToSend, axiosConfig)
+        await axios.post(url, this.dataToSend, axiosConfig)
         this.message = "Successfully stored data"
       }
       catch (error) {
@@ -90,5 +144,22 @@ button {
 }
 .default.info {
   color: #99A;
+}
+div.row {
+  display: flex;
+}
+div.option {
+  flex: 5 5;
+  background: peachpuff;
+  margin: 0 0.5em;
+  font-weight: bold;
+  padding: 0.5em;
+  text-align: center;
+  transition: background 0.1s ease-out;
+  border: 2px solid orange;
+}
+div.option.selected {
+  background: lightskyblue;
+  border: 2px solid navy;
 }
 </style>
