d6030ce00ea3ff079c45e8024b42805edfdf140f
calisaurus <github@calisaurus.net>
2021-12-22 13:49:35 +0000

    Add in basic error handling

--- docs/.vuepress/components/CreatePlayRecords.vue
+++ docs/.vuepress/components/CreatePlayRecords.vue
@@ -45,7 +45,8 @@
         <br />
         <b>Data preview</b>
         <pre>{{JSON.stringify({ title, date, coop, winner, noOfPlayers }, null, 2)}}</pre>
-      </div>
+        <p v-if="message">{{message}}</p>
+      </div>    
 </template>
 
 <script>
@@ -57,11 +58,12 @@ export default {
       date: '',
       coop: '',
       winner: '',
-      noOfPlayers: ''
+      noOfPlayers: '',
+      message: ''
     }
   },
   methods: {
-    handleSubmit(event) {
+    async handleSubmit(event) {
       event.preventDefault()
       console.log(`You played ${this.title} on ${this.date}. ${this.winner} was the winner!`)
       const dataToSend = {
@@ -71,7 +73,15 @@ export default {
         winner: this.winner,
         noOfPlayers: this.noOfPlayers,
       }
-      axios.post('https://32r96w1hxa.execute-api.eu-west-2.amazonaws.com/Prod/createPlayRecord', dataToSend)
+      try {
+        console.log('Do the thing')
+        await axios.post('https://32r96w1hxa.execute-api.eu-west-2.amazonaws.com/Prod/createPlayRecord', dataToSend)
+        this.message = "Successfully stored data"
+      }
+      catch (error) {
+        this.message = error.message
+        console.error("Could not post to endpoint", error)
+      }
     }
   }
 }
