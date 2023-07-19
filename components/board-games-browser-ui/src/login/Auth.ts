44df88a40206378895c86b196a9110c4dda1702a
John Beech <johnbeech.github@mkv25.net>
2023-07-19 23:10:29 +0100

    Fix redirect and postLogoutRedirectUri values

--- components/board-games-browser-ui/src/login/Auth.ts
+++ components/board-games-browser-ui/src/login/Auth.ts
@@ -21,9 +21,9 @@ const prodIdentityDetails = {
 const makeOAuthClient = memoizee(function (identityConfig: OAuthClientOptions) {
   const authClient = createOAuthClient({
     ...identityConfig,
-    redirectUri: `${window.location.origin}/`, // Return to execute auth handler code
+    redirectUri: `${window.location.origin}/boardgames-browser/`, // Return to execute auth handler code
     useLocalLoginRedirectUrl: true, // Return users back to the page they were viewing before auth
-    postLogoutRedirectUri: `${window.location.origin}/`
+    postLogoutRedirectUri: `${window.location.origin}/boardgames-browser/`
   })
   authClient.initialize()
     .catch((err) => {
